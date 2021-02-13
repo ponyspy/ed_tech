c5034a096a3e8c6fbc0b6eb64fc7dc85261e3151
ponyspy <desade4me@gmail.com>
2021-02-13 20:59:13 +0300

    init

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 $(function() {
 
-	$('.section_item').on('mouseenter', function() {
-		$('.section_item').removeClass('active').filter(this).addClass('active');
+	$('.header_block').marquee({
+		//duration in milliseconds of the marquee
+		duration: 10000,
+		//gap in pixels between the tickers
+		gap: 40,
+		//time in milliseconds before the marquee will start animating
+		delayBeforeStart: 0,
+		//'left' or 'right'
+		direction: 'left',
+		//true or false - should the marquee be duplicated to show an effect of continues flow
+		duplicated: false
 	});
 
 	var draw_el = $('.draw')[0];
@@ -27,6 +36,10 @@ $(function() {
 
 		this.href = data;
 		this.download = 'draw';
+	});
+
+	$('.section_item').on('click', function() {
+		$('.section_item').removeClass('active').filter(this).addClass('active');
 	});
 
 });
